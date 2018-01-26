90eb6ed5e83df19bf50c2ab685adb10707505c18
Sunset-wkshp <amber37@mail.usf.edu>
2018-01-26 16:10:06 -0500

    Changed Form to FlaskForm to avoid depreciation

--- forms.py
+++ forms.py
@@ -1,20 +1,20 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Email, Length, NumberRange
 
-class SignupForm(Form):
+class SignupForm(FlaskForm):
     first_name = StringField('First name', validators=[DataRequired("Put YO Name")])
     last_name = StringField('Last name', validators=[DataRequired("PUT YOUR LAST NAMAE")])
     email = StringField('Email', validators=[DataRequired("I won't spam you I promise"), Email("PUT A REAL EMAIL")])
     password = PasswordField('Password', validators=[DataRequired("DO IT"), Length(min=6, message="Think harder man")])
     submit = SubmitField('Sign up')
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired("Please enter an email"), Email("put a real email")])
     password = PasswordField('Password', validators=[DataRequired("Enter your password")])
     submit = SubmitField("Sign in")
 
-class PatientForm(Form):
+class PatientForm(FlaskForm):
     first_name = StringField('First name', validators=[DataRequired("Enter first name")])
     last_name = StringField('Last Name', validators=[DataRequired("Enter last name")])
     age = IntegerField('Age', validators=[DataRequired("Enter your age"), NumberRange(min=0, max=100, message="Enter an age between 0-100")])
