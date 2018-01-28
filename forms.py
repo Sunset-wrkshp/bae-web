9e26fea840f177d57c3195081070809dea84c82b
Sunset-wkshp <amber37@mail.usf.edu>
2018-01-27 21:54:48 -0500

    Created DB, modified form to fit parameters

--- forms.py
+++ forms.py
@@ -24,5 +24,8 @@ class PatientForm(FlaskForm):
     #how to be gender inclusive while still specifiying body type
     #how to check so ONLY M or F is put in
     #make custom validator for checking M or F
+    #OK so this isnt necessary at the moment, but could be added later.
     sex = StringField('Sex', validators=[DataRequired("Enter your biological sex"), Length(min=0, max=1, message="Enter M or F")], description="Enter M or F for bioloical sex, not gender.")
+    s_pulse = IntegerField('Starting Pulse', validators=[DataRequired("Enter your starting heart rate"), NumberRange(min=0)], description="Take starting pulse (count how many beats you get in 10 seconds then multiply by 6")
+    e_pulse = IntegerField('Ending Pulse', validators=[DataRequired("Enter your ending heart rate"), NumberRange(min=0)], description="Take ending pulse immediately after exercise (count how many beats you get in 10 seconds then multiply by 6)" )
     submit = SubmitField('Complete')
